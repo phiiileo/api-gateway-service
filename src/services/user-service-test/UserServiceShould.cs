f8475bd3753c1f6ebc3d4791eaf96ee935531578
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-09-02 22:08:18 +0100

    added user-service project and user-service-test project

---
+++ src/services/user-service-test/UserServiceShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace user_service_test
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void UserServiceShould()
+        {
+
+        }
+    }
+}
